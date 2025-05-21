8bccc4a76eec93693f55f4298a669b05c7038849
Bibek Koirala <www.koiralabibek2058@gmail.com>
2025-05-21 11:26:36 +0545

    done with type

--- typescript/index.ts
+++ typescript/index.ts
@@ -58,3 +58,33 @@ interface Reactangle extends Line {
 // used with as keywords
 
 // Things should be converted to unknown before typecasting
+class Bibek {
+  private name!: string;
+}
+
+abstract class Polygon {
+  public abstract getArea(): number;
+
+  public toString(): string {
+    return `Polygon[area=${this.getArea()}]`;
+  }
+}
+
+class Rectangle extends Polygon {
+  public constructor(
+    protected readonly width: number,
+    protected readonly height: number
+  ) {
+    super();
+  }
+
+  public getArea(): number {
+    return this.width * this.height;
+  }
+}
+
+const myRect = new Rectangle(10, 20);
+
+console.log(myRect.getArea());
+
+// Generics
